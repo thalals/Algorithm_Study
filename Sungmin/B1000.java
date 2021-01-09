ea1f138cc7635f6c6090114cbbda65ecb9b026b3
songmin9813 <mithan1@naver.com>
2021-01-09 17:32:17 +0900

    Create BOJ1000_한성민

---
+++ Sungmin/B1000.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class B1000 {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		int x;
+		int y;
+		x=input.nextInt();
+		y=input.nextInt();
+		System.out.println(x+y);
+		input.close();
+	}
+}
