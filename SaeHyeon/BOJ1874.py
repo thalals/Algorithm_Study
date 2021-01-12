c555a13a2bd58c3c271d59da78dcf99fcb8e7f48
jeonhl7579 <tpgus19@gmail.com>
2021-01-12 14:26:51 +0900

    2021-01-12 BOJ1874

---
+++ SaeHyeon/BOJ1874.py
@@ -0,0 +1,28 @@
+
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+cnt=1
+array=[]
+stack=[]
+result=True
+for i in range(0,n):
+    m=int(input())
+    while cnt <= m:
+        array.append(cnt)
+        cnt+=1
+        stack.append("+")
+        print("+")
+    if array[-1] == m:
+        array.pop()
+        stack.append("-")
+        print("-")
+    else:
+        result=False
+
+if result == False:
+    print("NO")
+else:
+    for i in range(0,len(stack)):
+        print(stack[i])
