12c09d8a2afe7d9b49f0a0263f24e7e987c89b4c
jeonhl7579 <tpgus19@gmail.com>
2021-01-13 14:16:53 +0900

    create BOJ10845_전세현

---
+++ SaeHyeon/BOJ10845.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+dq=deque()
+
+n=int(input())
+
+for i in range(0,n):
+    ins=input().split()
+    if ins[0] == "push":
+        dq.append(ins[1])
+    elif ins[0] == "pop":
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq.popleft())
+    elif ins[0] == "size":
+        print(len(dq))
+    elif ins[0] == "empty":
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif ins[0] == "front":
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[0])
+    elif ins[0] == "back":
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[len(dq)-1])
+
