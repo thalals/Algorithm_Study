fbb7f83380643016f1bf6f0b732519274c9d194d
jeonhl7579 <tpgus19@gmail.com>
2021-01-13 15:18:43 +0900

    create BOJ1158_전세현

---
+++ SaeHyeon/BOJ1158.py
@@ -0,0 +1,24 @@
+# 시간 초과
+import sys
+
+input=sys.stdin.readline
+
+a,b=map(int,input().split())
+
+arr=[i+1 for i in range(0,a)]
+index=1
+stack=[]
+arr.reverse()
+
+while True:
+    if len(arr) == 0:
+        break
+    else:
+        if index == b:
+            stack.append(arr.pop())
+            index=1
+        else:
+            arr.insert(0,arr.pop())
+            index+=1
+
+print("<%s>" %(", ".join(map(str,stack))))
