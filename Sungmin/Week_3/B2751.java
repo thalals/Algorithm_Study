359e1eaae221efc01663480c81e1b98d73061f8c
songmin9813 <mithan1@naver.com>
2021-01-27 17:18:47 +0900

    Update BOJ2750_한성민 using Dual-Pivot Quicksort

--- Sungmin/Week_3/B2751.java
+++ Sungmin/Week_3/B2751.java
@@ -1,23 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 public class B2751 {
 	public static void main(String[] args) {
 		try {
 			BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
 			StringBuilder sb = new StringBuilder();
 			int x=Integer.parseInt(input.readLine());
-			ArrayList<Integer> nums = new ArrayList<Integer>();
-			for(int i=0;i<x;i++) {
-				int temp = Integer.parseInt(input.readLine());
-				nums.add(temp);
-			}
-			Collections.sort(nums);
-			for(int i=0;i<x;i++) {
-				sb.append(nums.get(i)+"\n");
-			}
+			int[] nums = new int[x];
+			for(int i=0;i<x;i++) nums[i]=Integer.parseInt(input.readLine());
+			Arrays.sort(nums);
+			for(int i=0;i<x;i++) sb.append(nums[i]+"\n");
 			System.out.print(sb);
 			input.close();
 		}catch(IOException e) {
