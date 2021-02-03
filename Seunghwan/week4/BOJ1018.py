a8259f1e6334fe142ceac8f0fd727cb9db16deee
seunghw <ysp1307@naver.com>
2021-02-03 21:15:06 +0900

    [UPD] 폴더 생성

---
+++ Seunghwan/week4/BOJ1018.py
@@ -0,0 +1,15 @@
+n, m = map(int,input().split())
+
+board = []
+
+for i in range(n):
+    board.append(input())
+
+
+for i in range(n-7):
+    for j in range(m-7):
+        cnt1 = 0
+        cnt2 = 0
+
+
+    
