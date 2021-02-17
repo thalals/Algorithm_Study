0ec268b32935a7a7797c5428401563501c498857
songmin9813 <mithan1@naver.com>
2021-02-17 20:11:12 +0900

    Update BOJ7576_한성민 solving timeout error by using bfs

--- Sungmin/Week_5/B7576.java
+++ Sungmin/Week_5/B7576.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 public class B7576 {
 	static StringBuilder sb = new StringBuilder();
 	static boolean visited[][];
 	static int dayVisited[][];
 	static int tomato[][];
-	static int daycount=0;
+	static int daycount=-1;
 	static boolean changed;
 	public static void main(String[] args) {
 		try {
@@ -23,44 +25,49 @@ public class B7576 {
 					tomato[i][i1]=Integer.parseInt(temp[i1-1]);
 				}
 			}//토마토 입력 배열
-			while(true) {
-				changed=false;
-				for(int i=1;i<vertex+1;i++) {
-					for(int i1=1;i1<y+1;i1++) {
-						if(tomato[i][i1]==1&&dayVisited[i][i1]==daycount) {
-							dfs(i,i1,vertex,y);
-						}
-					}
-				}
-				if(!changed) break;
-				else daycount++;
-			}//검사
+			Queue<Integer> qx = new LinkedList<Integer>();
+			Queue<Integer> qy = new LinkedList<Integer>();
+			int dx[] = {0,1,0,-1};
+			int dy[] = {1,0,-1,0};
 			for(int i=1;i<vertex+1;i++) {
 				for(int i1=1;i1<y+1;i1++) {
-					if(tomato[i][i1]==0) {
-						System.out.print("-1");
-						return;
+					if(tomato[i][i1]==1&&!visited[i][i1]) {
+						dayVisited[i][i1]=1;
+						visited[i][i1]=true;
+						qx.add(i);
+						qy.add(i1);
 					}
 				}
-			}//-1판단
-			System.out.print(daycount);
+			}//처음 도마도만 큐에 담음
+			while(!qx.isEmpty()) {
+				int X=qx.poll();
+				int Y=qy.poll();
+				for(int i2=0;i2<4;i2++) {
+					int newX=X+dx[i2];
+					int newY=Y+dy[i2];
+					if(newX>=1&&newX<=vertex&&newY>=1&&newY<=y&&!visited[newX][newY]&&tomato[newX][newY]==0) {
+						tomato[newX][newY]=1;
+						visited[newX][newY]=true;
+						qx.add(newX);
+						qy.add(newY);
+						if(dayVisited[newX][newY]==0) dayVisited[newX][newY]=dayVisited[X][Y]+1;
+						else if(dayVisited[newX][newY]>dayVisited[X][Y]+1) dayVisited[newX][newY]=dayVisited[X][Y]+1;
+					}
+				}
+			}
+			for(int i=1;i<vertex+1;i++) {
+				for(int i1=1;i1<y+1;i1++) {
+					if(!visited[i][i1]&&tomato[i][i1]==0) {
+						System.out.print("-1");
+						System.exit(0);
+					}
+					else if(dayVisited[i][i1]>daycount) daycount=dayVisited[i][i1];
+				}
+			}
+			System.out.print(daycount-1);
 			input.close();
 		}catch(IOException e) {
 			e.printStackTrace();
-		}
-	}
-	public static void dfs(int x, int y, int sizeX, int sizeY) {
-		visited[x][y]=true;
-		int dx[] = {0,1,0,-1};
-		int dy[] = {1,0,-1,0};
-		for(int i=0;i<4;i++) {
-			int newX=x+dx[i];
-			int newY=y+dy[i];
-			if(newX>=1&&newX<=sizeX&&newY>=1&&newY<=sizeY&&!visited[newX][newY]&&tomato[newX][newY]==0) {
-				changed=true;
-				dayVisited[newX][newY]=daycount+1;
-				tomato[newX][newY]=1;
-			}	
 		}
 	}
 }
