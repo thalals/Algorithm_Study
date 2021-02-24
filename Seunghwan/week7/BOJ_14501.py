4e2236d8667cca4badfa7ccbbcd5907824ee2613
seunghwan Kim <ysp1307@naver.com>
2021-02-24 22:10:11 +0900

    [UPD] BOJ14501 해결

--- Seunghwan/week7/BOJ_14501.py
+++ Seunghwan/week7/BOJ_14501.py
@@ -1,13 +1,27 @@
+# dp를 찾아보던 중 브루트포스로 푼 정답을 찾음
+# 이해만 하면 상당히 괜찮다고 생각함
+
 n = int(input())
 time = []
 pay = []
-
-'''
-다이나믹 프로그래밍에 대해 공부해야할듯하다
-
-
-
+ans = 0
 
 for _ in range(n):
-    
-'''
+    t,p = map(int,input().split())
+    time.append(t)
+    pay.append(p)
+
+
+def sol(index, profit):
+    global ans
+
+    if index > n: # index 가 n을 넘은 경우 종료
+        return
+    if index == n: # index+t[i]가 n보다 작은 경우 계속
+        ans = max(ans, profit)
+        return
+    sol(index + 1, profit)
+    sol(index + time[index], profit + pay[index])
+
+sol(0, 0)
+print(ans)
