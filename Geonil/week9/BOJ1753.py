8a971a40e6a9035f44924283044d5e7b4fa24651
Gun1Yun <rjsdlf45@gmail.com>
2021-03-12 02:21:36 +0900

    [ADD] BOJ1753(해결)

--- Geonil/week9/BOJ1753.py
+++ Geonil/week9/BOJ1753.py
@@ -3,34 +3,26 @@ import heapq
 from collections import defaultdict
 In = sys.stdin.readline
 
-INF = float('inf')
-
 num_v, num_e = map(int, In().split())
 start_v = int(In())
 
 
 class Graph:
     def __init__(self, n):
-        self.graph = [[INF]*n for _ in range(n)]
+        self.graph = [[] for _ in range(n)]
         self.num_vertex = n
-
-        for i in range(n):
-            for j in range(n):
-                if i == j:
-                    self.graph[i][j] = 0
 
     def set_graph(self, num_e):
         for _ in range(num_e):
             i, j, w = map(int, In().split())
-            self.graph[i-1][j-1] = min(self.graph[i-1][j-1], w)
+            self.graph[i-1].append([j-1, w])
 
     def daijkstra(self, start_vertex):
         self.distance = [float('inf')]*self.num_vertex
         self.distance[start_vertex] = 0
 
         hq = []
-        for idx, dist in enumerate(self.distance):
-            heapq.heappush(hq, (dist, idx))
+        heapq.heappush(hq, (0, start_vertex))
 
         while hq:
             dist, idx = heapq.heappop(hq)
@@ -38,11 +30,11 @@ class Graph:
             if dist < self.distance[idx]:
                 continue
 
-            for i in range(self.num_vertex):
-                comp_dist = self.distance[idx] + self.graph[idx][i]
-                if comp_dist < self.distance[i]:
-                    heapq.heappush(hq, (comp_dist, i))
-                    self.distance[i] = comp_dist
+            for v, w in self.graph[idx]:
+                comp_dist = self.distance[idx] + w
+                if comp_dist < self.distance[v]:
+                    heapq.heappush(hq, (comp_dist, v))
+                    self.distance[v] = comp_dist
 
         for item in self.distance:
             if item == float('inf'):
