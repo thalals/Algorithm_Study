8a40a928864f9633a46d85f258e1a97c4d79f69a
Gun1Yun <rjsdlf45@gmail.com>
2021-03-30 17:22:44 +0900

    [UPD] BOJ19238(진행)

--- Geonil/week11/BOJ19238.py
+++ Geonil/week11/BOJ19238.py
@@ -17,70 +17,109 @@ for _ in range(n):
     maps.append(sub)
 taxi = list(map(int, In().split()))
 
-passengers = []
-for _ in range(m):
+dest = [[] for _ in range(m+2)]
+for i in range(m):
     tmp = list(map(int, In().split()))
-    passenger = [tmp[:2], tmp[2:]]
-    passengers.append(passenger)
-passengers = sorted(passengers, key=lambda x: (x[0][0], x[0][1]))
+    maps[tmp[0]-1][tmp[1]-1] = i+2
+    dest[i+2] = [tmp[2], tmp[3]]
 
 
-def check_pos(x, y):
+def check(x, y):
     if 0 <= x-1 < len(maps) and 0 <= y-1 < len(maps):
         if maps[x-1][y-1] != 1:
             return True
+
     return False
 
 
-def bfs(now, target):
+def search_passenger(now):
     visited = []
-
     queue = deque()
     queue.append([now, 0])
     visited.append(now)
 
     while queue:
-        pos, distance = queue.popleft()
+        length = len(queue)
+        person = []
 
-        if pos == target:
+        for _ in range(length):
+            position, distance = queue.popleft()
+            x, y = position
+
+            if maps[x-1][y-1] > 1:
+                person.append([x, y])
+
+            for move in moves:
+                dx, dy = move
+                new_x, new_y = x+dx, y+dy
+                if check(new_x, new_y) and [new_x, new_y] not in visited:
+                    queue.append([[new_x, new_y], distance + 1])
+                    visited.append([new_x, new_y])
+
+        if person:
+            person.sort()
+            return person[0], distance
+
+    return -1, -1
+
+
+def move_taxi(now, target):
+    visited = []
+    queue = deque()
+    queue.append([now, 0])
+    visited.append(now)
+
+    while queue:
+        position, distance = queue.popleft()
+        x, y = position
+        if [x, y] == target:
             return distance
 
         for move in moves:
-            x, y = pos
             dx, dy = move
             new_x, new_y = x+dx, y+dy
-            if [new_x, new_y] not in visited:
-                if check_pos(new_x, new_y):
-                    queue.append([[new_x, new_y], distance+1])
-                    visited.append([new_x, new_y])
+            if check(new_x, new_y) and [new_x, new_y] not in visited:
+                queue.append([[new_x, new_y], distance+1])
+                visited.append([new_x, new_y])
+
+    return -1
 
 
 def main():
     global taxi
     global fuel
+    global m
+
+    cnt = 0
 
     while True:
-        distance = 100
-        for idx, passenger in enumerate(passengers):
-            d = bfs(taxi, passenger[0])
-            if d < distance:
-                distance = d
-                p = idx
+        if cnt == m:
+            break
 
+        person, distance = search_passenger(taxi)
+        if distance == -1:
+            print(-1)
+            return
+
+        x, y = person
+        destination = maps[x-1][y-1]
+        maps[x-1][y-1] = 0
         fuel -= distance
-        use = bfs(taxi, passengers[idx][1])
-        fuel -= use
-        taxi = passengers[idx][1]
+        taxi = [x, y]
 
-        passengers.remove(passengers[p])
+        use = move_taxi(taxi, dest[destination])
+        if use == -1:
+            print(-1)
+            return
+
+        fuel -= use
+        taxi = dest[destination]
 
         if fuel < 0:
             break
 
+        cnt += 1
         fuel += use*2
-
-        if not passengers:
-            break
 
     if fuel < 0:
         print(-1)
