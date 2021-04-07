ecb3ac856651e1efb3afe6b60ed177de7e49e2af
songmin9813 <mithan1@naver.com>
2021-04-07 13:50:18 +0900

    [ADD] BOJ16235(해결)

--- Sungmin/Week_13/B16235.java
+++ Sungmin/Week_13/B16235.java
@@ -1,13 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Deque;
 
 public class B16235 {
 	static ArrayList<Tree> trees=new ArrayList<Tree>();
 	static ArrayList<Tree> ageFive=new ArrayList<Tree>();
 	static ArrayList<Integer> index=new ArrayList<Integer>();
+	static Deque<Tree> treesArray[][];
 	static int result;
 	static class Tree implements Comparable<Tree>{
 		int x;
@@ -30,30 +33,37 @@ public class B16235 {
 		
 	}
 	
-	static void spring(int[][] board) {
-		for(int i=0;i<trees.size();i++) {
-			if(trees.get(i).died) continue;
-			if(board[trees.get(i).x][trees.get(i).y]>=trees.get(i).age) {
-				//먹을 수 있음
-				board[trees.get(i).x][trees.get(i).y]-=trees.get(i).age++;
-				if(trees.get(i).age%5==0)//다섯쨜은 가을에 써야됨
-					ageFive.add(trees.get(i));
-			}
-			else {
-				//못먹으면 뒤짐
-				index.add(i);
+	static void spring(int[][] board,int n) {
+		for(int i=1;i<=n;i++) {
+			for(int i1=1;i1<=n;i1++) {
+				Deque<Tree> temp= new ArrayDeque<Tree>();//생존한 나무 넣기
+				while(!treesArray[i][i1].isEmpty()) {
+					temp.add(treesArray[i][i1].pollFirst());
+					if(board[i][i1]>=temp.peekLast().age) {
+						//먹을 수 있음
+						board[i][i1]-=temp.peekLast().age++;
+						if(temp.peekLast().age%5==0)
+							ageFive.add(temp.peekLast());
+					}
+					else {
+						//못먹음. 탐색한 거 포함 뒤에거 다죽음
+						trees.add(temp.pollLast());
+						while(!treesArray[i][i1].isEmpty())
+							trees.add(treesArray[i][i1].pollFirst());
+					}
+				}
+				treesArray[i][i1]=temp;
 			}
 		}
 	}
 	
 	static void summer(int[][] board) {
-		for(int i=0;i<index.size();i++) {
-			int deleteindex=index.get(i);
-			board[trees.get(deleteindex).x][trees.get(deleteindex).y]+=(trees.get(deleteindex).age/2);
-			trees.get(deleteindex).died=true;
+		for(int i=0;i<trees.size();i++) {
+			Tree temp=trees.get(i);
+			board[temp.x][temp.y]+=(temp.age/2);
 			result--;
 		}
-		index.clear();
+		trees.clear();
 	}
 	
 	static void autumn(int n) {//번식
@@ -65,7 +75,7 @@ public class B16235 {
 				int newX=temp.x+dx[i1];
 				int newY=temp.y+dy[i1];
 				if(newX>=1&&newX<=n&&newY>=1&&newY<=n) {//유효성 검사
-					trees.add(0, new Tree(newX,newY,1));
+					treesArray[newX][newY].addFirst(new Tree(newX,newY,1));
 					result++;
 				}
 			}
@@ -88,17 +98,17 @@ public class B16235 {
 		int m=Integer.parseInt(s[1]);//나무 개수
 		int k=Integer.parseInt(s[2]);//몇년 트리거
 		int[][] board=new int[n+1][n+1];//땅
-		ArrayList<Tree> trees[][]=new ArrayList[n+1][n+1];
+		treesArray=new ArrayDeque[n+1][n+1];
 		int[][] foodBoard=new int[n+1][n+1];//거름 정보
 		for(int i=1;i<=n;i++) {
 			s=input.readLine().split(" ");
 			for(int i1=1;i1<=n;i1++) {
 				foodBoard[i][i1]=Integer.parseInt(s[i1-1]);
 				board[i][i1]=5;
-				trees[i][i1]=new ArrayList<Tree>();
+				treesArray[i][i1]=new ArrayDeque<Tree>();
 			}
 		}
-		/*for(int i=0;i<m;i++) {
+		for(int i=0;i<m;i++) {
 			s=input.readLine().split(" ");
 			int x=Integer.parseInt(s[0]);
 			int y=Integer.parseInt(s[1]);
@@ -106,18 +116,22 @@ public class B16235 {
 			trees.add(new Tree(x,y,age));
 		}
 		Collections.sort(trees);
+		for(int i=0;i<m;i++) {
+			Tree temp = trees.get(i);
+			treesArray[temp.x][temp.y].add(temp);
+		}
 		result+=trees.size();
-		//입력완료
+		//입력완료. 이제 trees는 죽은 나무들을 관리함
+		trees.clear();
 		
 		int count=0;
 		while(count!=k) {
-			spring(board);
+			spring(board,n);
 			summer(board);
 			autumn(n);
 			winter(board, foodBoard, n);
 			count++;
-		}*/
-		
+		}
 		
 		System.out.print(result);
 		input.close();
