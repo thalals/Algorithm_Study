db908f3583a27b6623a9ef7c3c3f45aad16a453e
Gun1Yun <rjsdlf45@gmail.com>
2021-04-09 00:50:38 +0900

    [ADD] BOJ12865(해결)

---
+++ Geonil/week13/BOJ12865.py
@@ -0,0 +1,24 @@
+import sys
+In = sys.stdin.readline
+
+
+def main():
+    n, k = map(int, In().split())
+    items = []
+
+    for _ in range(n):
+        items.append(list(map(int, In().split())))
+
+    lst = [0] * (k+1)
+
+    for w, v in items:
+        for i in range(k, 0, -1):
+            print(i)
+            if w <= i:
+                lst[i] = max(lst[i-w] + v, lst[i])
+
+    print(lst[k])
+
+
+if __name__ == "__main__":
+    main()
