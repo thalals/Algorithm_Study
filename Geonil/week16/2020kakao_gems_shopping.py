c57f8eaca8a7e364c47f2533e81382fc3ecb391e
Gun1Yun <rjsdlf45@gmail.com>
2021-04-29 20:22:20 +0900

    [UPD] kakao_gemshopping(해결)

--- Geonil/week16/2020kakao_gems_shopping.py
+++ Geonil/week16/2020kakao_gems_shopping.py
@@ -6,49 +6,44 @@ In = sys.stdin.readline
 gems = list(map(str, In().rstrip().split()))
 
 
-def check_gems(gem_dict: dict) -> bool:
-    for key in gem_dict:
-        if gem_dict[key] == 0:
-            return False
-    return True
-
-
 def main():
     gem = set(gems)
-    gem_dict = dict()
-    for jewelry in gem:
-        gem_dict[jewelry] = 0
+    gem_dict = dict()   # dict for count each gem
 
-    length = len(gems)
+    size = len(gem)     # number of gems
+    length = len(gems)  # length of gems list
 
     left = 0
     right = 0
     gem_dict[gems[left]] = 1
-    min_section = None
+    min_section = [0, 100000]   # max size of answer
 
-    if len(gem) == 1:
+    if size == 1:
         print("[1, 1]")
     else:
         while True:
-            if right == length:
-                break
-
-            if check_gems(gem_dict):
-                if min_section:
-                    min_section = min(min_section, [left+1, right+1],
-                                      key=lambda x: x[1]-x[0])
-                else:
-                    min_section = [left+1, right+1]
+            # if number of gem == size of gem
+            if size == len(gem_dict):
+                # check section length
+                min_section = min(min_section, [left+1, right+1],
+                                  key=lambda x: x[1]-x[0])
 
                 gem_dict[gems[left]] -= 1
+
+                if gem_dict[gems[left]] == 0:
+                    del gem_dict[gems[left]]
                 left += 1
             else:
                 right += 1
 
+                # if right pointer == length of gems list, break loop
                 if right == length:
                     break
-
-                gem_dict[gems[right]] += 1
+                # if right pointer's gem is not in gems_dict
+                if gems[right] not in gem_dict:
+                    gem_dict[gems[right]] = 1
+                else:
+                    gem_dict[gems[right]] += 1
 
         print(min_section)
 
