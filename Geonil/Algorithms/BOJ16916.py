6a31cd0a444edf4a7492380fa61b1c9e9a651fce
Gun1Yun <rjsdlf45@gmail.com>
2021-05-06 19:00:28 +0900

    [ADD] BOJ16916(해결)

--- Geonil/Algorithms/BOJ16916.py
+++ Geonil/Algorithms/BOJ16916.py
@@ -4,21 +4,45 @@ import sys
 In = sys.stdin.readline
 
 
+def get_pi(string: str) -> list:
+    length = len(string)
+    pi = [0] * length
+    idx = 0
+
+    for i in range(1, length):
+        while idx > 0 and string[i] != string[idx]:
+            idx = pi[idx-1]
+
+        if string[i] == string[idx]:
+            idx += 1
+            pi[i] = idx
+
+    return pi
+
+
+def kmp(string: str, sub: str) -> bool:
+    pi = get_pi(sub)
+    len_main = len(string)
+    len_sub = len(sub)
+    idx = 0
+
+    for i in range(len_main):
+        while idx > 0 and string[i] != sub[idx]:
+            idx = pi[idx-1]
+
+        if string[i] == sub[idx]:
+            if idx == len_sub-1:
+                return True
+            else:
+                idx += 1
+
+    return False
+
+
 def main():
     string = In().rstrip()
     sub = In().rstrip()
-    length = len(sub)
-    flag = False
-
-    for i in range(len(string)-length+1):
-        if not sub[0] == string[i]:
-            continue
-
-        if string[i:i+length] == sub:
-            flag = True
-            break
-
-    if flag:
+    if kmp(string, sub):
         print(1)
     else:
         print(0)
