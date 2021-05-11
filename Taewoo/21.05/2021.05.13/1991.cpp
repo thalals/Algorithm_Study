ffd60510ad7111b20e84ef0404411813759b8cbf
bn-tw2020 <skaxodn97@gmail.com>
2021-05-11 16:25:14 +0900

    [Add] 3005(해결)

--- Taewoo/21.05/2021.05.13/1991.cpp
+++ Taewoo/21.05/2021.05.13/1991.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string result[3];
 int tree[27][2];
 int N;
-// https://velog.io/@embeddedjune/VSCode-WSL2%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-CC-%EC%BB%B4%ED%8C%8C%EC%9D%BC%EB%B9%8C%EB%93%9CGDB-%EB%94%94%EB%B2%84%EA%B9%85-%EB%B0%A9%EB%B2%95-%EB%A9%94%EB%AA%A8
+
 void preorder(int idx) {
     result[0] += char(idx + 'A' - 1);
     if(tree[idx][0]) preorder(tree[idx][0]);
