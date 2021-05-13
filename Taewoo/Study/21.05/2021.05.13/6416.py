51c1b17fae9003152b7853f53e516876b90b282c
bn-tw2020 <skaxodn97@gmail.com>
2021-05-13 18:21:05 +0900

    [UPD] 6416

--- Taewoo/Study/21.05/2021.05.13/6416.py
+++ Taewoo/Study/21.05/2021.05.13/6416.py
@@ -2,42 +2,49 @@ import sys
 input = sys.stdin.readline
 
 
-def is_tree(pairs: list, c_num: int) -> str:
-    if len(pairs) == 1:
-        if pairs[0][0] == pairs[0][1]:
-            return f"Case {c_num} is not a tree."
-        return f"Case {c_num} is a tree."
-    if len(pairs) == 0:
-        return f"Case {c_num} is a tree."
-    childs = dict()
-    nodes = set()
-    for pair in pairs:
-        u, v = pair[0], pair[1]
-        if pair[1] in childs:
-            return f"Case {c_num} is not a tree."
-        childs[v] = u
-        nodes.add(u)
-        nodes.add(v)
-    if len(nodes) == len(pairs) + 1:
-        return f"Case {c_num} is a tree."
-    else:
-        return f"Case {c_num} is not a tree."
+def is_tree():
+    is_root = False
+    for i in range(1, len(tree)):
+        if is_root and node_check[i] and len(tree[i]) == 0:
+            return False
+        if not is_root and node_check[i] and len(tree[i]) == 0:
+            is_root = True
+        if node_check[i] and len(tree[i]) > 1:
+            return False
+
+    if not is_root:
+        return False
+
+    return True
 
 
-k = 0
+# main
+k = 1
 while True:
-    case = []
-    flag = 1
+    nodes = []
+    is_end = False
+
+    while True:
+        numbers = list(map(int, input().split()))
+        if len(numbers) != 0 and numbers[-1] == 0 and numbers[-2] == 0:
+            nodes.extend(numbers)
+            break
+        if len(numbers) != 0 and numbers[-1] == -1 and numbers[-2] == -1:
+            sys.exit(0)
+        if len(numbers) == 0:
+            continue
+        nodes.extend(numbers)
+
+    tree = [[] for _ in range(max(nodes) + 1)]
+    node_check = [False for _ in range(max(nodes) + 1)]
+    for i in range(0, len(nodes) - 1, 2):
+        tree[nodes[i + 1]].append(nodes[i])
+        node_check[nodes[i]] = True
+        node_check[nodes[i + 1]] = True
+
+    if len(tree) == 1 or is_tree():
+        print(f'Case {k} is a tree.')
+    else:
+        print(f'Case {k} is not a tree.')
+
     k += 1
-    while flag:
-        for c in input().rstrip().split('  '):
-            if c == "":
-                continue
-            c = tuple(map(int, c.split()))
-            if c[0] == -1 and c[1] == -1:
-                sys.exit()
-            if c[0] == 0 and c[1] == 0:
-                print(is_tree(case, k))
-                flag = 0
-                break
-            case.append(c)
