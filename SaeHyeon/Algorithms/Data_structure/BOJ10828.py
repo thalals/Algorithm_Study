2eb3ff5e2763f67c2ba882ca5e93e7a13f42fb73
jeonhl7579 <tpgus19@gmail.com>
2021-06-17 15:27:10 +0900

    [ADD] BOJ10828(해결)

---
+++ SaeHyeon/Algorithms/Data_structure/BOJ10828.py
@@ -0,0 +1,35 @@
+import sys
+input=sys.stdin.readline
+
+
+def stack(arr,str):
+    if str[0] == "push":
+        arr.append(str[1])
+    if str[0] == "pop":
+        if not arr:
+            print(-1)
+        else:
+            print(arr.pop())
+    if str[0] == "size":
+        print(len(arr))
+    if str[0] == "empty":
+        if len(arr) == 0:
+            print(1)
+        else:
+            print(0)
+    if str[0] == "top":
+        if len(arr) == 0:
+            print(-1)
+        else:
+            print(arr[len(arr)-1])
+
+
+def main():
+    n=int(input())
+    arr=[]
+    for i in range(n):
+        s=input().split()
+        stack(arr,s)
+
+if __name__ == "__main__":
+    main()
--- SaeHyeon/Algorithms/Data_structure/BOJ1158.py
+++ SaeHyeon/Algorithms/Data_structure/BOJ1158.py
@@ -1,3 +1,5 @@
+#Problem : https://www.acmicpc.net/problem/1158
+#Comment : 결과는 맞게 나왔는데 틀림, 나중에 다시 풀어보기
 from collections import deque
 
 n,k=map(int,input().split())
