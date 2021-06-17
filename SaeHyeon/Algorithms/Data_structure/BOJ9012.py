73f741e32a4eb06f1c8b397b8680b37216b3841f
jeonhl7579 <tpgus19@gmail.com>
2021-06-17 15:56:08 +0900

    [ADD] BOJ9012(해결)

---
+++ SaeHyeon/Algorithms/Data_structure/BOJ9012.py
@@ -0,0 +1,29 @@
+import sys
+input=sys.stdin.readline
+
+def yes_no(s):
+    stack=[]
+    cnt=0
+    for i in s:
+        if i=='(':
+            stack.append(i)
+            cnt+=1
+        if i == ')':
+            if stack:
+                stack.pop()
+            cnt-=1
+            if cnt == -1:
+                return 'NO'
+    if cnt == 0:
+        return 'YES'
+    else:
+        return 'NO'
+
+def main():
+    t=int(input())
+    for i in range(t):
+        s=input().rstrip()
+        print(yes_no(s))
+
+if __name__ == "__main__":
+    main()
