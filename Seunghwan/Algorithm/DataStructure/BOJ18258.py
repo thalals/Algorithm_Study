7457fe86bc2c2a18d251b11410de2d4038e6e371
Seunghwan <ysp1307@naver.com>
2021-06-22 18:06:50 +0900

    [ADD] BOJ18258(해결)

--- Seunghwan/Algorithm/DataStructure/BOJ18258.py
+++ Seunghwan/Algorithm/DataStructure/BOJ18258.py
@@ -1,7 +1,9 @@
 import sys
+from collections import deque
+
 n = int(sys.stdin.readline())
 
-result = []
+result = deque([])
 
 for i in range(n):
     command = sys.stdin.readline().split()
@@ -10,30 +12,30 @@ for i in range(n):
         result.append(command[1])
 
     elif command[0] == "pop":
-        if len(result) == 0:
+        if not result:
             print(-1)
         else:
-            print(result.pop())
+            print(result.popleft())
     
     elif command[0] == "size":
         print(len(result))
 
     elif command[0] == "empty":
-        if len(result) == 0:
+        if not result: # 스택에선 오른쪽처럼 쓰지만 deque에선 왼쪽처럼 작성함. if len(result) == 0:
             print(1)
         else:
             print(0)
     
     elif command[0] == "front":
-        if len(result) == 0:
-            print(-1)
-        else:
-            print(result[-1])
-    elif command[0] == "back":
-        if len(result) == 0:
+        if not result:
             print(-1)
         else:
             print(result[0])
+    elif command[0] == "back":
+        if not result:
+            print(-1)
+        else:
+            print(result[-1])
          
         
     
