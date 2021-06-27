ed29b4ada919a6718a70a94a77a41b175ccfa8c2
jeonhl7579 <tpgus19@gmail.com>
2021-06-27 15:14:22 +0900

    [ADD]BOJ11279(해결)

--- SaeHyeon/Algorithms/Data_structure2/BOJ11279.py
+++ SaeHyeon/Algorithms/Data_structure2/BOJ11279.py
@@ -1,17 +1,20 @@
+#Problem : https://www.acmicpc.net/problem/11279
+#Comment : 해결
+
 import sys
+import heapq
+
 input=sys.stdin.readline
+heap=[]
 
-arr=[]
 n=int(input())
-
 for i in range(n):
     a=int(input())
     if a == 0:
-        if len(arr) == 0:
+        if len(heap) == 0:
             print(0)
             continue
-        max_value=max(arr)
-        print(arr.pop(arr.index(max_value)))
-    else:
-        arr.append(a)
+        print(heapq.heappop(heap)[1])        
+    heapq.heappush(heap,(-a,a))
 
+#print(heap)
