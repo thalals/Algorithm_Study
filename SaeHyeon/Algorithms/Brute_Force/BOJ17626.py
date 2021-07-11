dc84e48a07dcdf606f21cb8160036333e16e4771
jeonhl7579 <tpgus19@gmail.com>
2021-07-11 14:11:05 +0900

    [ADD]BOJ17626(해결)

--- SaeHyeon/Algorithms/Brute_Force/BOJ17626.py
+++ SaeHyeon/Algorithms/Brute_Force/BOJ17626.py
@@ -1,16 +1,33 @@
 import sys
-from itertools import permutations
-input=sys.stdin.readline
+from math import sqrt
+input=sys.stdin.readline().rstrip
+
+
+def solve(n):
+    # ret가 횟수제한(자연수를 넷 홋은 그 이하의 제곱수 합으로 나타내는 것이기 때문)
+    ret=4
+    for a in range(1,sqrtN+1):
+        if a*a==n:
+            ret=1
+
+        if ret==1:
+            break
+        for b in range(1,sqrtN+1):
+            if a*a+b*b>n:
+                break
+            if a*a+b*b==n:
+                ret=2
+            
+            if ret <=2:
+                break
+            for c in range(1,sqrtN+1):
+                if a*a+b*b+c*c>n:
+                    break
+                if a*a+b*b+c*c==n:
+                    ret=3
+                    break
+    return ret
 
 n=int(input())
-k=int(input())
-
-li=set()
-arr=[]
-for _ in range(n):
-    arr.append(input().rstrip())
-
-for a in permutations(arr,k):
-    li.add(''.join(a))
-
-print(len(li))
+sqrtN=int(sqrt(n))
+print(solve(n))
