3394af4c344960239e17b4fe5ebb937d9ad44975
jeonhl7579 <tpgus19@gmail.com>
2021-07-15 17:08:24 +0900

    [ADD]BOJ15654(해결)

---
+++ SaeHyeon/Algorithms/back_tracking/BOJ15654.py
@@ -0,0 +1,25 @@
+import sys
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+
+number=sorted(list(map(int,input().split())))
+visited=[False]*N
+arr=[]
+
+def dfs(cnt):
+    if cnt == M:
+        print(*arr)
+        return
+    
+    for i in range(N):
+        if visited[i]:
+            continue
+
+        visited[i]=True
+        arr.append(number[i])
+        dfs(cnt+1)
+        arr.pop()
+        visited[i]=False
+
+dfs(0)
