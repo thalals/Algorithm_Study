a61ab5b7823489182be23ae7e282ec11a9717c89
jeonhl7579 <tpgus19@gmail.com>
2021-07-15 16:59:57 +0900

    [ADD]BOJ15651(해결)

--- SaeHyeon/Algorithms/back_tracking/BOJ15651.py
+++ SaeHyeon/Algorithms/back_tracking/BOJ15651.py
@@ -17,12 +17,10 @@ def dfs(cnt):
             continue
 
         #visited[i]=True
-        for j in range(i):
-            visited[j]=True
         arr.append(number[i])
         dfs(cnt+1)
         arr.pop()
-        for z in range(i+1,N):
-            visited[z]=False
+        # for j in range(i+1,N):
+        #     visited[j]=False
 
 dfs(0)
