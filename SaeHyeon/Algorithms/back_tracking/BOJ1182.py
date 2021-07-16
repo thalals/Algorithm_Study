c9a92202c248d6c9e546b04d1aa297c62310339b
jeonhl7579 <tpgus19@gmail.com>
2021-07-16 13:20:52 +0900

    [ADD]BOJ1182(수정)

--- SaeHyeon/Algorithms/back_tracking/BOJ1182.py
+++ SaeHyeon/Algorithms/back_tracking/BOJ1182.py
@@ -7,9 +7,22 @@ arr=list(map(int,input().split()))
 
 cnt=0
 
-for i in range(1,N+1):
-    for asd in combinations(arr,i):
-        if sum(asd) == S:
-            cnt+=1
+# for i in range(1,N+1):
+#     for asd in combinations(arr,i):
+#         if sum(asd) == S:
+#             cnt+=1
 
+# print(cnt)
+
+# 백트래킹
+def dfs(idx,result):
+    global cnt
+    if idx>=N:
+        return
+    result+=arr[idx]
+    if result == S:
+        cnt+=1
+    dfs(idx+1,result-arr[idx])
+    dfs(idx+1,result)
+dfs(0,0)
 print(cnt)
