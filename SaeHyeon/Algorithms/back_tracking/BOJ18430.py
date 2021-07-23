5bba85e7201c48509e7e3078ca81f847c310ea3e
jeonhl7579 <tpgus19@gmail.com>
2021-07-23 16:29:25 +0900

    [UPD]BOJ18430(진행)

--- SaeHyeon/Algorithms/back_tracking/BOJ18430.py
+++ SaeHyeon/Algorithms/back_tracking/BOJ18430.py
@@ -1,3 +1,6 @@
+# 문제 : 백준 18430
+# 내용 : 런타임에러
+
 import sys
 input=sys.stdin.readline
 
@@ -6,25 +9,65 @@ namu=[]
 for _ in range(M):
     namu.append(list(map(int,input().split())))
 visit=[[False]*M for _ in range(N)]
-max_result=0
+all_sum=0
 def dfs(y,x,value):
-    global max_result
+    global all_sum
     
     if x==M:
         x=0
         y+=1
     if y==N:
+        all_sum=max(all_sum,value)
         return
-    if y+1<N and x+1<M and not visit[y+1][x] and not visit[y][x+1]:
+        
+    # a a
+    # a
+    if y+1<N and x+1<M and not visit[y+1][x] and not visit[y][x+1] and not visit[y][x]:
         visit[y][x]=True
         visit[y+1][x]=True
         visit[y][x+1]=True
-        value=(namu[y][x]*2)+namu[y+1][x]+namu[y][x+1]
-        max_result=max(max_result,value)
-        dfs(y,x+1,max_result)
+        result=value+(namu[y][x]*2)+namu[y+1][x]+namu[y][x+1]
+        dfs(y,x+1,result)
         visit[y][x]=False
         visit[y+1][x]=False
         visit[y][x+1]=False
+    
+    #a a
+    #  a
+    if y+1<N and x-1>=0 and not visit[y+1][x] and not visit[y][x-1] and not visit[y][x]:
+        visit[y][x]=True
+        visit[y+1][x]=True
+        visit[y][x-1]=True
+        result=value+((namu[y][x]*2)+namu[y+1][x]+namu[y][x-1])
+        dfs(y,x+1,result)
+        visit[y][x]=False
+        visit[y+1][x]=False
+        visit[y][x-1]=False
         
+    #   a
+    # a a
+    if y-1>=0 and x-1>=0 and not visit[y-1][x] and not visit[y][x-1] and not visit[y][x]:
+        visit[y][x]=True
+        visit[y-1][x]=True
+        visit[y][x-1]=True
+        result=value+((namu[y][x]*2)+namu[y-1][x]+namu[y][x-1])
+        dfs(y,x+1,result)
+        visit[y][x]=False
+        visit[y-1][x]=False
+        visit[y][x-1]=False
+    
+    # a
+    # a a
+    if y-1>=0 and x+1<M and not visit[y-1][x] and not visit[y][x+1] and not visit[y][x]:
+        visit[y][x]=True
+        visit[y-1][x]=True
+        visit[y][x+1]=True
+        result=value+((namu[y][x]*2)+namu[y-1][x]+namu[y][x+1])
+        dfs(y,x+1,result)
+        visit[y][x]=False
+        visit[y-1][x]=False
+        visit[y][x+1]=False
+    dfs(y,x+1,value)
+
 dfs(0,0,0)
-print(max_result)
+print(all_sum)
