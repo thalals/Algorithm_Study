6f5a23c42d350a0abb18bb54b6685f6128edd4f0
thalals <thalsal@naver.com>
2021-08-11 00:02:20 +0900

    [ADD] boj2578

--- Hyeongmin/Algorithm/src/implementation/boj2578_빙고.java
+++ Hyeongmin/Algorithm/src/implementation/boj2578_빙고.java
@@ -37,18 +37,38 @@ public class boj2578_빙고 {
 		}
 		
 		int count=0;
+		int result=0;
 		//call number
 		for(int i=0;i<5;i++) {
 			temp = br.readLine().split(" ");
 			for(String a : temp) {
-				count++;
 				int number = Integer.parseInt(a);
-				
+				result++;
 				//숫자 지우기(방문)
 				dot n = position.get(number);
 				visit[n.x][n.y] = true;
 				
 				//그 숫자로부터 행 열 대각선 빙고 확인
+				//2,2 는 대각선도 확인
+				if(bingo1(n.x, n.y) )
+					count++;
+				if(bingo2(n.x, n.y) )
+					count++;
+				//대각선 상에 있을 때
+				if(n.x-n.y==0) {
+					if(bingo3(n.x, n.y))
+						count++;
+				}
+				if(n.y+n.x==4) {
+					if(bingo4(n.x, n.y))
+						count++;
+				}
+				
+				if(count>=3) {
+					System.out.println(result);
+					return;
+				}
+					
 				
 			}
 		}
@@ -56,21 +76,44 @@ public class boj2578_빙고 {
 	}
 	
 	//빙고 확인
-	public static boolean bingo(int x, int y) {
+	public static boolean bingo1(int x, int y) {
 		//행
 		for(int i=0;i<5;i++) {
 			if(!visit[x][i])
 				return false;
 		}
-		
+		return true;
+	}
+	public static boolean bingo2(int x, int y) {
 		//열
 		for(int i=0;i<5;i++) {
 			if(!visit[i][y])
 				return false;
 		}
+		return true;
+	}
+	
+	public static boolean bingo3(int x, int y) {
+		//왼쪽대각선
+		x=0;
+		y=0;
+		for(int i=0;i<5;i++) {
+			if(!visit[x++][y++])
+				return false;
+		}
+		return true;
+
+	}
+	public static boolean bingo4(int x, int y) {
+		//오른쪽 대각선 x+1 y-1
 		
-		//대각선
-		for(int)
+		x=0;
+		y=4;
+		for(int i=0;i<5;i++) {
+			if(!visit[x++][y--])
+				return false;
+		}
+
 		return true;
 	}
 }
